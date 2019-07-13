2394f53dcaeb931d5c56f297d5e7c63ac7b5c627
wtser <wangtieshou@gmail.com>
2019-07-13 09:59:34 +0800

    Add qr input

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import kjua from "kjua";
 chrome.tabs.getSelected(null, function(tab) {
   const $qr = document.getElementById("qr"),
     $qrFav = document.getElementById("qr_fav"),
-    $qrFavImg = document.getElementById("qr_fav_img");
+    $qrFavImg = document.getElementById("qr_fav_img"),
+    $qrInput = document.getElementById("qrInput");
   $qr.innerHTML = "";
+  $qrInput.value = tab.url;
   $qr.appendChild(kjua({ text: tab.url, size: 240 }));
 
-  if (tab.favIconUrl) {
-    if (tab.favIconUrl.substr(0, 4) == "http") {
-      $qrFavImg.src = tab.favIconUrl;
-      $qrFav.style.display = "block";
-    }
+  if (tab.favIconUrl && tab.favIconUrl.substr(0, 4) == "http") {
+    $qrFavImg.src = tab.favIconUrl;
+    $qrFav.style.display = "block";
   }
+
+  $qrInput.onclick = e => {
+    e.target.select();
+  };
 });
